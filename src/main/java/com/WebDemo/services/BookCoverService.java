98b5c3519b1bde5a1be7e647f3daddb659d9d32a
Akbirov Bulat <36545325+baton4ik2@users.noreply.github.com>
2024-09-03 22:53:30 +0300

    Update BookCoverService.java

--- src/main/java/com/WebDemo/services/BookCoverService.java
+++ src/main/java/com/WebDemo/services/BookCoverService.java
@@ -69,7 +69,7 @@ public class BookCoverService {
             ByteArrayOutputStream baos = new ByteArrayOutputStream()){
             BufferedImage image = ImageIO.read(bis);
 
-            int height = image.getHeight() / (image.getWidth() / 100);
+            int height = image.getHeight() / (image.getWidth() / 50);
             BufferedImage preview = new BufferedImage(100, height, image.getType());
             Graphics2D graphics = preview.createGraphics();
             graphics.drawImage(image, 0, 0, 100, height, null);
